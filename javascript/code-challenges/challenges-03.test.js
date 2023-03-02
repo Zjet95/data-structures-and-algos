f4ff9fe2724b3aa823202dd2f37c87715133180f
Zachariah Jeter <Zachjeter95@icloud.com>
2023-03-01 23:44:51 -0800

    added verbage

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -68,6 +68,7 @@ const notInFirstArray = (forbiddenValues, arr) => {
   return newArr;
 };
 
+//note added to complete pull RQ
 
 
 /* ------------------------------------------------------------------------------------------------
